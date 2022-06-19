aae0379c14583b0d38d81e4be5639b651b2babbc
WolbachAuth0 <aaron.wolbach@okta.com>
2022-06-18 23:40:03 -0400

    altered path tags

--- server/openapi-docs/tags.js
+++ server/openapi-docs/tags.js
@@ -1,27 +1,18 @@
-let data = [
-  {
-    name: 'Auth',
-    description: `Methods for authenticating and authorizing users.`,
-    group: 'Users'
-  },
-  {
-    name: 'Administration',
-    description: `Methods for administrators who need to manage the user database.`,
-    group: 'Users'
-  }
-]
-
 module.exports = {
-  tags: data.map(x => {
-    return {
-      name: x.name,
-      description: x.description
+  tags: [
+    {
+      name: 'Custom DB Action',
+      description: `Methods for authenticating and authorizing users.`,
+    },
+    {
+      name: 'Administration',
+      description: `Methods for administrators who need to manage the user database.`,
     }
-  }),
+  ],
   'x-tagGroups': [
     {
       name: 'Users',
-      tags: data.filter(x => x.group === 'Users').map(x => x.name)
+      tags: [ 'Custom DB Action', 'Administration' ]
     },
   ]
 }
