f06b797cb464308ce6645d60ed5526b42583ca46
WolbachAuth0 <aaron.wolbach@okta.com>
2022-06-18 23:40:42 -0400

    consolodated users paths

--- server/openapi-docs/paths/users.js
+++ server/openapi-docs/paths/users.js
@@ -1,9 +1,11 @@
-const { content } = require('../helpers')
+const { content } = require('./../helpers')
+const requestBodies = require('./../components/requestBodies')
+const parameters = require('./../components/parameters')
 
 module.exports = {
   '/api/v1/users/login': {
     get: {
-      tags: [ 'Auth' ],
+      // tags: [ 'Custom DB Action' ],
       summary: 'Login User',
       description: `Attempts to log a user in. User credentials are extracted from the 
       Basic Auth Scheme. The basusername (email) and password.`,
@@ -12,19 +14,119 @@ module.exports = {
       ],
       responses: {
         '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/User' }) },
+        '401': { '$ref': '#/components/responses/401' },
+      },
+      // 'x-code-samples': sample.get('/catalog')
+    }
+  },
+  '/api/v1/users/search': {
+    get: {
+      // tags: [ 'Custom DB Action' ],
+      summary: 'Search Users',
+      description: `Find a list users which match the query parameters passed in the url`,
+      security: [
+        { Access_Token: [] }
+      ],
+      parameters: [
+        { '$ref': '#/components/parameters/email' },
+        { '$ref': '#/components/parameters/populate' },
+        { '$ref': '#/components/parameters/limit' },
+        { '$ref': '#/components/parameters/page' },
+      ],
+      responses: {
+        '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/Paginated' }) },
+        '400': { '$ref': '#/components/responses/400' },
         '401': { '$ref': '#/components/responses/401' }, // unauthorized
       },
       // 'x-code-samples': sample.get('/catalog')
     }
   },
-
-  '/api/v1/users': {
+  '/api/v1/users/{id}': {
     get: {
-      tags: [ 'Auth' ],
-      summary: '',
-      description: `Find a list users which match the query parameters passed in the url`,
+      tags: [ 'Administration' ],
+      summary: 'Find User by ID',
+      description: `Finds a User document that has the passed id.`,
+      security: [
+        { Access_Token: [] }
+      ],
+      parameters: [
+        { '$ref': '#/components/parameters/id' },
+        // { '$ref': '#/components/parameters/populate' },
+      ],
+      responses: {
+        '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/User' }) },
+        '401': { '$ref': '#/components/responses/401' }, // unauthorized
+        '404': { '$ref': '#/components/responses/404' }, // not found
+      },
+      // 'x-code-samples': sample.get('/catalog/prod')
+    },
+    put: {
+      tags: [ 'Administration' ],
+      summary: 'Update User',
+      description: `Describe the columns of the table with the passed table name.`,
+      security: [
+        { Access_Token: [] }
+      ],
+      parameters: [
+        { '$ref': '#/components/parameters/id' },
+      ],
+      requestBody: { '$ref': '#/components/requestBodies/UpdateUser' },
       responses: {
         '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/Paginated' }) },
+        '400': { '$ref': '#/components/responses/400' }, // bad request
+        '401': { '$ref': '#/components/responses/401' }, // unauthorized
+        '404': { '$ref': '#/components/responses/404' }, // not found
+        // '500': { '$ref': '#/components/responses/500' }
+      },
+      // 'x-code-samples': sample.get('/catalog/prod')
+    },
+    delete: {
+      // tags: [ 'Custom DB Action' ],
+      summary: 'Delete User',
+      description: `Removes the single quote associated to the passed id from the database.`,
+      security: [
+        { Access_Token: [] }
+      ],
+      parameters: [
+        { '$ref': '#/components/parameters/id' },
+      ],
+      responses: {
+        '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/Paginated' }) },
+        '401': { '$ref': '#/components/responses/401' }, // unauthorized
+        '404': { '$ref': '#/components/responses/404' }, // not found
+      },
+      // 'x-code-samples': sample.get('/catalog/prod')
+    },
+  },
+  '/api/v1/users/verify': {
+    put: {
+      // tags: [ 'Custom DB Action' ],
+      summary: 'Verify Email',
+      description: `Verifies a user's email address.`,
+      security: [
+        { Access_Token: [] }
+      ],
+      requestBody: { '$ref': '#/components/requestBodies/VerifyEmail' },
+      responses: {
+        '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/User' }) },
+        '400': { '$ref': '#/components/responses/400' }, // bad request
+        '401': { '$ref': '#/components/responses/401' }, // unauthorized
+        '404': { '$ref': '#/components/responses/404' }, // not found
+      },
+      // 'x-code-samples': sample.get('/catalog')
+    }
+  },
+  '/api/v1/users/change-password': {
+    put: {
+      // tags: [ 'Custom DB Action' ],
+      summary: 'Change Password',
+      description: `Changes the password of the user associated to the email address in the request body.`,
+      security: [
+        { Access_Token: [] }
+      ],
+      requestBody: { '$ref': '#/components/requestBodies/ChangePassword' },
+      responses: {
+        '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/User' }) },
         '400': { '$ref': '#/components/responses/400' }, // bad request
         '401': { '$ref': '#/components/responses/401' }, // unauthorized
         '404': { '$ref': '#/components/responses/404' }, // not found
@@ -34,26 +136,17 @@ module.exports = {
   },
   '/api/v1/users/signup': {
     post: {
-      tags: [ 'Auth' ],
+      // tags: [ 'Custom DB Action' ],
       summary: 'Sign Up User',
-      description: `Signs up a new user with the email and password provided in the basic auth.`,
+      description: `Signs up a new user with the email and password provided in the request body.`,
       security: [
-        { Basic_Auth: [] }
+        { Access_Token: [] }
       ],
-      requestBody: {
-        description: 'User to add to the system',
-        required: true,
-        content: {
-          'application/json': {
-            schema: { $ref: '#/components/schemas/User' },       
-          },
-        },
-      },
+      requestBody: { '$ref': '#/components/requestBodies/SignupUser' },
       responses: {
         '200': { description: 'OK', content: content({ '$ref': '#/components/schemas/User' }) },
         '400': { '$ref': '#/components/responses/400' }, // bad request
         '401': { '$ref': '#/components/responses/401' }, // unauthorized
-        '404': { '$ref': '#/components/responses/404' }, // not found
       },
       // 'x-code-samples': sample.get('/catalog')
     }
